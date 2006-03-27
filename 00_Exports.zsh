d5798cb74b32c320f8fd4cb9b763653bb771457c
hugues <hugues@a0e5b806-a6f9-0310-978d-cbce73f8a913>
2006-03-27 21:46:27 +0000

    Mise à jour des scripts de démarrage de ZSH
    git-svn-id: svn+ssh://hugues@maison/opt/svn/private/hugues@183 a0e5b806-a6f9-0310-978d-cbce73f8a913

--- 00_Exports.zsh
+++ 00_Exports.zsh
@@ -1,17 +1,19 @@
 export EDITOR="vim"
 export HISTFILE=".zsh/.history.zsh"
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:~/libs
-eval $(dircolors ~/.dir_colors)
+eval $(dircolors ~/.dir_colors 2>&-)
 export PATH=$PATH:~/sbin:~/bin
 export PKG_CONFIG_PATH=/usr/X11R6/lib/pkgconfig
 #export PRINTER=Gertrude
 export PRINTER=Berthe
 export TIME_STYLE="+%Y-%b-%d %H:%M"
 export TZ="Europe/Paris"
-source ~/.keychain/$(hostname)-sh
+KEYCHAIN=~/.keychain/$(hostname)-sh
+if ( [ -f $KEYCHAIN ] ); then
+	source $KEYCHAIN
+fi
 export PATH=$PATH:~/.pr0n
 # Set locales only if they are undefined
 export LC_ALL=${LC_ALL:-fr_FR.UTF-8}
 export LC_MESSAGES=${LC_MESSAGES:-fr_FR}
 unset LANG # Unuseful
-export MPS1="[34mMuZzik[1m> [0m"
---
+++ 98_Music.zsh
@@ -0,0 +1,2 @@
+export MPS1="[1m%s[0m > "
+export MPS2="[34mMuZzik[0;1m %c [0m"
