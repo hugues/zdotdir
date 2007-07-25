3404a0365568df2d290145a674762174002b88a5
hugues <hugues@a0e5b806-a6f9-0310-978d-cbce73f8a913>
2007-07-25 10:15:07 +0000

    Gestion de la couleur s'il y a des trucs à committer sous svn :)
    git-svn-id: svn+ssh://hugues@maison/opt/svn/private/hugues@502 a0e5b806-a6f9-0310-978d-cbce73f8a913

--- 11_Prompts.zsh
+++ 11_Prompts.zsh
@@ -90,8 +90,8 @@ precmd ()
 	DATE=$(date "+%H:%M:%S-%d/%m/%Y")
 	ERROR=%(? "---" "%3<<"$C_$COLOR_BAR$_C"--"$C_$COLOR_ERRR$_C"%?%<<")
 	GITBRANCH=${$(git branch 2>&-):+$C_$COLOR_DOUBLEDOT$_C:$C_$COLOR_BRANCH$_C$(git branch | grep '^\* ' | cut -c3-)}
-	if [ $(svn status | grep '^M' | wc -l) -gt 0 ] ; then COLOR_BRANCH=$COLOR_BRANCH\;$UNDERLINE ; fi
-	SVNREV=${$(svn info 2>&-):+$C_$COLOR_DOUBLEDOT$_C:$C_$COLOR_BRANCH$_C"r$(svn info | tail -n+5 | head -n1 | cut -d' ' -f3)"}
+	if [ $(svn status | grep '^M' | wc -l) -gt 0 ] ; then COLOR_STATUS=$COLOR_BRANCH\;$UNDERLINE ; else COLOR_STATUS=$COLOR_BRANCH ; fi
+	SVNREV=${$(svn info 2>&-):+$C_$COLOR_DOUBLEDOT$_C:$C_$COLOR_STATUS$_C"r$(svn info | tail -n+5 | head -n1 | cut -d' ' -f3)"}
 	CURDIR=$C_$COLOR_PATH$_C"%(!.%d.%(5~:.../:)%4~)"$C_$VOID$_C"$GITBRANCH$SVNREV"
 	unset HBAR
 	for _hbar in {1..$(($COLUMNS - ${#DATE} - 3 - 2))}
