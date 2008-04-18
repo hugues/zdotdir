bf9cfc39bb27e569f0bc96ab74d4bc75d74c4cb8
Hugues Hiegel <hugues.hiegel@sagem.com>
2008-04-18 12:01:31 +0200

    Environnement de dev..

---
+++ net:eurodec/Environment.zsh
@@ -0,0 +1,9 @@
+export m_SVIM=/users/picard/local/share/vim/syntax
+export m_VIM_USER_NAME="H. Hiegel"
+
+hash -d data=/data/$USER
+hash -d target=/targets/$USER
+
+export KERNEL_PATH=~data/forge/kernel
+export COMP="-C $KERNEL_PATH"
+export TARGET_PATH=~target
