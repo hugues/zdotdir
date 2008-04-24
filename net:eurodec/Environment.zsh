e2f9a952558aea3a37296f45dc911f109342748a
Hugues Hiegel <hugues.hiegel@sagem.com>
2008-04-24 14:36:12 +0200

    "Workspaces" are called "Worksets" here...

--- net:eurodec/Environment.zsh
+++ net:eurodec/Environment.zsh
@@ -4,7 +4,7 @@ export m_VIM_USER_NAME="H. Hiegel"
 export LOGCHECK=0 # Don't want to get fucked up by this trick...
 
 hash -d data=/data/$USER
-hash -d workspace=~data/workspace
+hash -d workset=~data/workset
 hash -d targets=/targets/$USER
 
 export TARGET_PATH=~targets/target
