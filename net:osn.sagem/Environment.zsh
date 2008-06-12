1a0b3979930861b6f8eabbcc9f4d6bd675ab957b
Hugues Hiegel <hugues@hiegel.fr>
2008-06-12 15:52:10 +0200

    Renamed hash dirs to their original Disc Letters..

--- net:osn.sagem/Environment.zsh
+++ net:osn.sagem/Environment.zsh
@@ -1,6 +1,6 @@
 
 export PATH=~/local/bin:~/local/sbin:$PATH
 
-hash -d K=/media/osn01001/Projets_STB
-hash -d J=/media/osn02001/Echanges
+hash -d K:=/media/osn01001/Projets_STB
+hash -d J:=/media/osn02001/Echanges
 
