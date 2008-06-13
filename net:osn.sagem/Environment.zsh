8b963df1ffa7f51468607bd58444035d0f114de9
Hugues Hiegel <hugues@hiegel.fr>
2008-06-13 14:59:50 +0200

    Dumped config from net:eurodec to net:osn.sagem.

--- net:osn.sagem/Environment.zsh
+++ net:osn.sagem/Environment.zsh
@@ -1,6 +1,18 @@
+#--------[ Hashes ]-------------------------------------------------
+hash -d data=~/data
+hash -d targets=~/targets
 
-export PATH=~/local/bin:~/local/sbin:$PATH
+#--------[ Generic Config ]-----------------------------------------
+eval $(~data/sagem-script44/set-env.pl 2>/dev/null) && source ~/.env
 
+#--------[ Specific Config ]----------------------------------------
+export TARGET_PATH=~targets/target
+export KERNEL_PATH_BASE=~/Kernel/exports/kernel-
+export COMP=ARCH=sh\ CROSS_COMPILE=sh4-linux-uclibc-
+export STLINUXROOT=/opt/STM/STLinux-2.2/devkit/sh4/
+
+export PATH=$PATH:$STLINUXROOT/bin
+
+#--------[ Shared Folders ]-----------------------------------------
 hash -d K:=/media/osn01001/Projets_STB
 hash -d J:=/media/osn02001/Echanges
-
