3c853e73f9ecb5a31ef188c1d1d69921ff078acc
Hugues Hiegel <hugues@hiegel.fr>
2008-09-30 10:52:26 +0200

    Environment update for osn.sagem

--- net:osn.sagem/Environment.zsh
+++ net:osn.sagem/Environment.zsh
@@ -9,9 +9,12 @@ eval $(~data/sagem-script44/set-env.pl 2>/dev/null) && source ~/.env
 export TARGET_PATH=~targets/target
 export KERNEL_PATH_BASE=~data/Kernel/kernel-
 export COMP=ARCH=sh\ CROSS_COMPILE=sh4-linux-uclibc-
-export STLINUXROOT=/opt/STM/STLinux-2.2/devkit/sh4/
+export STLINUXBASE=/opt/STM/STLinux-2.2
+export STLINUXROOT=$STLINUXBASE/devkit/sh4/
 
 export PATH=$PATH:$STLINUXROOT/bin
+export PATH=$PATH:$STLINUXBASE/host/bin
+export PATH=$PATH:/opt/STM/ST40R3.1.1_patch1/bin
 export PATH=~/local/bin:~/local/sbin:$PATH
 
 #--------[ Shared Folders ]-----------------------------------------
