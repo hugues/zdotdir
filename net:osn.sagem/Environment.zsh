0940b5ed3eb7bf3389e45ee8fa10ab298eb71a90
Hugues Hiegel <hugues@hiegel.fr>
2009-01-08 15:30:25 +0100

    [ENV] Update STLinux 2.3

--- net:osn.sagem/Environment.zsh
+++ net:osn.sagem/Environment.zsh
@@ -2,25 +2,32 @@
 [ "$LOGNAME" = "g178241" ] && HOME=~hugues
 
 #--------[ Hashes ]-------------------------------------------------
-hash -d data=~/Wr0k
+hash -d work=~/Wr0k
 hash -d targets=/targets
+hash -d tools=~work/t00lz
 
 #--------[ Generic Config ]-----------------------------------------
-eval $(~data/sagem-script44/set-env.pl 2>/dev/null) && source ~/.env
+eval $(~tools/sagem-script44/set-env.pl 2>/dev/null) && source ~/.env
 
 #--------[ Specific Config ]----------------------------------------
+export STLINUXRELEASE=${STLINUXRELEASE:-2.3}
+
 export TARGET_PATH=~targets/target
-export KERNEL_PATH_BASE=~data/Kernel/kernel-
-export COMP=ARCH=sh\ CROSS_COMPILE=sh4-linux-uclibc-
-export STLINUXBASE=/opt/STM/STLinux-2.2
+export KERNEL_PATH_BASE=~work/Kernel/exports/kernel-
+export COMP=ARCH=sh\ CROSS_COMPILE=sh4-linux-
+export STLINUXBASE=/opt/STM/STLinux-$STLINUXRELEASE
 export STLINUXROOT=$STLINUXBASE/devkit/sh4/
 
-export THIRDPARTYLIBS=~data/3rdparty/lib
+export THIRDPARTYLIBS=~work/3rdparty/lib
 
 export PATH=$PATH:$STLINUXROOT/bin
 export PATH=$PATH:$STLINUXBASE/host/bin
 export PATH=$PATH:/opt/STM/ST40R3.1.1_patch1/bin
 export PATH=~/local/bin:~/local/sbin:$PATH
+
+export FORGE=http://g178241@forge-urd44.osn.sagem/svn/
+
+export GET_KERNEL_METHOD=update
 
 #--------[ Shared Folders ]-----------------------------------------
 hash -d K:=/media/osn01001/Projets_STB
