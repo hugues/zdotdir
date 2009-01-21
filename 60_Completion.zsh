08b7c706cdc212dcdffc6179c156ae5f1cf36490
Hugues Hiegel <hugues@hiegel.fr>
2009-01-21 15:47:40 +0100

    [COMPLETION] removed everything since defaults values are ok..

--- 60_Completion.zsh
+++ 60_Completion.zsh
@@ -31,36 +31,36 @@ zstyle ':completion:*' format '%B---------------[ %d ]%b'
 
 # http://www.michael-prokop.at/computer/config/.zsh/zsh_completition
 
-zstyle ':completion:*:kill:*' command 'ps -u $USER -o pid,%cpu,tty,cputime,cmd'
-zstyle ':completion:*:killall:*' command 'ps -u $USER -o cmd'
-zstyle ':completion:*:kill:*' insert-ids single
-zstyle ':completion:*:*:kill:*' menu yes select
-zstyle ':completion:*:kill:*' force-list always
+#zstyle ':completion:*:kill:*' command 'ps -u $USER -o pid,%cpu,tty,cputime,cmd'
+#zstyle ':completion:*:killall:*' command 'ps -u $USER -o cmd'
+#zstyle ':completion:*:kill:*' insert-ids single
+#zstyle ':completion:*:*:kill:*' menu yes select
+#zstyle ':completion:*:kill:*' force-list always
 #zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#)*=0=01;31'
 
 # ssh/scp-completion
-zstyle ':completion:*:scp:*' tag-order \
-  'hosts:-host hosts:-domain:domain hosts:-ipaddr:IP\ address *'
-zstyle ':completion:*:scp:*' group-order \
-  users files all-files hosts-domain hosts-host hosts-ipaddr
-zstyle ':completion:*:ssh:*' tag-order \
-  users 'hosts:-host hosts:-domain:domain hosts:-ipaddr:IP\ address *'
-zstyle ':completion:*:ssh:*' group-order \
-  hosts-domain hosts-host users hosts-ipaddr
-zstyle ':completion:*:(ssh|scp):*:hosts-host' ignored-patterns \
-  '*.*' loopback localhost
-zstyle ':completion:*:(ssh|scp):*:hosts-domain' ignored-patterns \
-  '<->.<->.<->.<->' '^*.*' '*@*'
-zstyle ':completion:*:(ssh|scp):*:hosts-ipaddr' ignored-patterns \
-  '^<->.<->.<->.<->' '127.0.0.<->'
-zstyle ':completion:*:(ssh|scp):*:users' ignored-patterns \
-  adm bin daemon halt lp named shutdown sync
-zstyle -e ':completion:*:(ssh|scp):*' hosts 'reply=(
-  ${=${${(f)"$(cat {/etc/ssh_,~/.ssh/known_}hosts(|2)(N) \
-  /dev/null)"}%%[# ]*}//,/ }
-  ${=${(f)"$(cat /etc/hosts(|)(N) <<(ypcat hosts 2>/dev/null))"}%%\#*}
-  ${${${(M)${(s:# :)${(zj:# :)${(Lf)"$([[ -f ~/.ssh/config ]] && <~/.ssh/config)"}%%\#*}}##host(|name) *}#host(|name) }/\*}
-  )'
-#  ${(A)ssh_config_hosts:=${${${${(@M)${(f)"$(<~/.ssh/config)"}:#Host *}#Host }:#*\**}:#*\?*}}
-
-zstyle -e ':completion:*:quilt:*' command 'quilt help|tail -n +4|head -6'
+#zstyle ':completion:*:scp:*' tag-order \
+#  'hosts:-host hosts:-domain:domain hosts:-ipaddr:IP\ address *'
+#zstyle ':completion:*:scp:*' group-order \
+#  users files all-files hosts-domain hosts-host hosts-ipaddr
+#zstyle ':completion:*:ssh:*' tag-order \
+#  users 'hosts:-host hosts:-domain:domain hosts:-ipaddr:IP\ address *'
+#zstyle ':completion:*:ssh:*' group-order \
+#  hosts-domain hosts-host users hosts-ipaddr
+#zstyle ':completion:*:(ssh|scp):*:hosts-host' ignored-patterns \
+#  '*.*' loopback localhost
+#zstyle ':completion:*:(ssh|scp):*:hosts-domain' ignored-patterns \
+#  '<->.<->.<->.<->' '^*.*' '*@*'
+#zstyle ':completion:*:(ssh|scp):*:hosts-ipaddr' ignored-patterns \
+#  '^<->.<->.<->.<->' '127.0.0.<->'
+#zstyle ':completion:*:(ssh|scp):*:users' ignored-patterns \
+#  adm bin daemon halt lp named shutdown sync
+#zstyle -e ':completion:*:(ssh|scp):*' hosts 'reply=(
+#  ${=${${(f)"$(cat {/etc/ssh_,~/.ssh/known_}hosts(|2)(N) \
+#  /dev/null)"}%%[# ]*}//,/ }
+#  ${=${(f)"$(cat /etc/hosts(|)(N) <<(ypcat hosts 2>/dev/null))"}%%\#*}
+#  ${${${(M)${(s:# :)${(zj:# :)${(Lf)"$([[ -f ~/.ssh/config ]] && <~/.ssh/config)"}%%\#*}}##host(|name) *}#host(|name) }/\*}
+#  )'
+##  ${(A)ssh_config_hosts:=${${${${(@M)${(f)"$(<~/.ssh/config)"}:#Host *}#Host }:#*\**}:#*\?*}}
+#
+#zstyle -e ':completion:*:quilt:*' command 'quilt help|tail -n +4|head -6'
