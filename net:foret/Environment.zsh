41ebb8968480f78c5d33be0d51b1e02e50e59d0f
Hugues Hiegel <hugues.hiegel@qosmos.com>
2012-01-30 13:47:49 +0100

    [Env] Q-specific

--- net:foret/Environment.zsh
+++ net:foret/Environment.zsh
@@ -1,3 +1,8 @@
 
+# ccache
 export CCACHE_DIR=/work/$USER/.ccache
 
+# proxy
+export http_proxy="http://proxy.foret:3128"
+export ftp_proxy=$http_proxy
+export no_proxy="127.0.0.1,10.,.foret,.qosmos.com"
