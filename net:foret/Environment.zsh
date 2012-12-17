8e89e38630ebb66d23995bee2e8c26b3dd838fff
Hugues Hiegel <hugues.hiegel@qosmos.com>
2012-12-17 17:27:13 +0100

    [Net:Q] Get_Branches

--- net:foret/Environment.zsh
+++ net:foret/Environment.zsh
@@ -8,3 +8,13 @@ export CCACHE_DIR=/work/$USER/.ccache
 export http_proxy="http://proxy.foret:3128"
 export ftp_proxy=$http_proxy
 export no_proxy="127.0.0.1,10.,.foret,.qosmos.com"
+
+
+export GET_IXM_BRANCHES="if [ \\\"\$(dirname %p/%f)\\\" == \\\".\\\" ] ; then
+                             git clone work:%r -b %b \$( basename %p/%f ) ;
+                         else
+                             mkdir -p \$(dirname %p/%f) ; cd \$(dirname %p/%f ) ;
+                             git module add \$( basename %p/%f ) work:%r %b ;
+                             cd - ;
+                         fi"
+
