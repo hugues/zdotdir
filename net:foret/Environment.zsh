ae04f9ce9f61f871dc790019f9bd4c2957b49f8f
Hugues Hiegel <hugues.hiegel@qosmos.com>
2013-01-31 11:35:14 +0100

    [Q] Uses a shellscript for get-branches

--- net:foret/Environment.zsh
+++ net:foret/Environment.zsh
@@ -10,11 +10,5 @@ export ftp_proxy=$http_proxy
 export no_proxy="127.0.0.1,10.,.foret,.qosmos.com"
 
 
-export GET_IXM_BRANCHES="if [ \\\"\$(dirname %p/%f)\\\" == \\\".\\\" ] ; then
-                             git clone work:%r -b %b \$( basename %p/%f ) ;
-                         else
-                             mkdir -p \$(dirname %p/%f) ; cd \$(dirname %p/%f ) ;
-                             git module add \$( basename %p/%f ) work:%r %b ;
-                             cd - ;
-                         fi"
+export GET_IXM_BRANCHES="~/sbin/git_get.sh %p/%f %r %b"
 
