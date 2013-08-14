01120b410711cfc490c7a162a03c03ee56d4bf8b
Hugues Hiegel <hugues.hiegel@qosmos.com>
2013-08-14 11:23:16 +0200

    [Git] Get git status only if we got a branch…

--- 01_Internal.zsh
+++ 01_Internal.zsh
@@ -152,12 +152,14 @@ __get_git_fullstatus ()
 	local _branch _status _tracking _stashes
 
 	_branch=$(__get_git_branch)
-	_status=$(__get_git_branch_status)
 
-	[ -n "$_branch" ] && _branch=$C_$_prompt_colors[soft_generic]$_C${${_branch/→/$C_$_status$_C}/←/$C_$_prompt_colors[soft_generic]$_C}$C_$color[none]$_C
-
-	_tracking=$(__get_git_tracking_status)
-	_stashes=$(__get_git_stashes)
+	if [ -n "$_branch" ]
+	then
+		_status=$(__get_git_branch_status)
+		_branch=$C_$_prompt_colors[soft_generic]$_C${${_branch/→/$C_$_status$_C}/←/$C_$_prompt_colors[soft_generic]$_C}$C_$color[none]$_C
+		_tracking=$(__get_git_tracking_status)
+		_stashes=$(__get_git_stashes)
+	fi
 
 	[ -n "$1" ] && popd >/dev/null
 
