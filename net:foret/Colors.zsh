7cb8313b41045c77a796f53c95158d82e335b593
Hugues Hiegel <hugues.hiegel@qosmos.com>
2013-08-26 16:21:47 +0200

    [Q] Target : magenta → red

--- net:foret/Colors.zsh
+++ net:foret/Colors.zsh
@@ -1,7 +1,7 @@
 
 typeset -A _make_colors
 
-_make_colors[target]=$color[magenta]
+_make_colors[target]=$color[red]
 _make_colors[verbose]=$color[yellow]
 _make_colors[nproc]=$color[yellow]
 _make_colors[static]=$color[green]
