d0762e3fa173c28cf5b4469c2ddc25edaee28866
Hugues Hiegel <hugues.hiegel@qosmos.com>
2013-10-02 16:51:36 +0200

    [Env] Don’t display error when no tmux server running

--- net:foret/Environment.zsh
+++ net:foret/Environment.zsh
@@ -13,7 +13,7 @@ export no_proxy="127.0.0.1,10.,.foret,.qosmos.com"
 export GET_IXM_BRANCHES="~/sbin/git_get.sh %p/%f %r %b"
 
 # The following sets another std-user color for a specific tmux session
-if ( __cmd_exists tmux && tmux list-panes -F '#S #{pane_tty}' | grep -q "^pause $(tty)" )
+if ( __cmd_exists tmux && tmux list-panes -F '#S #{pane_tty}' 2>&- | grep -q "^pause $(tty)" )
 then
 	PS1_USER="31"
 fi
