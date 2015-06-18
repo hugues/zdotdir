f6a339ca9cbb06eee6865ce1a9658cf3dbb042c5
Hugues Hiegel <hugues.hiegel@anevia.com>
2015-06-18 17:22:48 +0200

    [Anevia] Specific funcs

--- net:anevia.com/user:hhiegel/Functions.zsh
+++ net:anevia.com/user:hhiegel/Functions.zsh
@@ -2,19 +2,30 @@
 __debian_dist() {
 
 	[ -e ~/.pbuilderrc ] || return
+	eval $(grep '^:' ~/.pbuilderrc)
 
-	_dist=${DIST:-$(awk < ~/.pbuilderrc '/DIST:=/ { gsub(/^.*{DIST:=/, "") ; gsub(/}$/, "") ; print ; exit }')}
-	print -Pn $C_${_env_colors[dist_$_dist]:-$_env_colors[dist_none]}
+	print -Pn $T_$_tu_$_T" "
+
+	#_dist=${DIST:-$(awk < ~/.pbuilderrc '/DIST:=/ { gsub(/^.*{DIST:=/, "") ; gsub(/}$/, "") ; print ; exit }')}
+	print -Pn $C_${_env_colors[dist_$DIST]:-$_env_colors[dist_none]}
 	( export | grep -q '^DIST=' ) && print -Pn ";"$color[bold]
 	print -Pn $_C
-	print -n $_dist
+	print -n $DIST
 
 	print -Pn $C_"38;5;33"$_C"-"
 
-	_base=${BASE:-$(awk < ~/.pbuilderrc '/BASE:=/ { gsub(/^.*{BASE:=/, "") ; gsub(/}$/, "") ; print ; exit }')}
-	print -Pn $C_${_env_colors[base_$_base]:-$_env_colors[base_none]}
+	#_base=${BASE:-$(awk < ~/.pbuilderrc '/BASE:=/ { gsub(/^.*{BASE:=/, "") ; gsub(/}$/, "") ; print ; exit }')}
+	print -Pn $C_${_env_colors[base_$BASE]:-$_env_colors[base_none]}
 	( export | grep -q '^BASE=' ) && print -Pn ";"$color[bold]
 	print -Pn $_C
-	print -n $_base
+	print -n $BASE
+
+	print -Pn $C_${_env_colors[arch_$ARCH]:-$_env_colors[arch_none]}
+	( export | grep -q '^ARCH=' ) && print -Pn ";"$color[bold]
+	print -Pn $_C
+	print -n " "$ARCH
+
+	print -Pn $C_$_prompt_colors[bar]$_C
+	print -Pn " "$T_$_tt_$_T
 }
 
--- net:anevia.com/user:hhiegel/Prompts.zsh
+++ net:anevia.com/user:hhiegel/Prompts.zsh
@@ -1,3 +1,13 @@
 
-PS1_TASKBAR+=(__debian_dist)
+function __Preload () {
+	local _preload=""
+	for lib in "$LD_PRELOAD"
+	do
+		_preload=${_preload:+$_preload }${${${lib:t}#lib}%%.*}
+	done
 
+	echo ${_preload:+$T_$_tu_$_T $_preload $T_$_tt_$_T}
+}
+PS1_TASKBAR+=(__Preload)
+
+PS1_TASKBAR+=(__debian_dist)
